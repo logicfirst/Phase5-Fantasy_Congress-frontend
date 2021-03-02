3be1b7ba150506ebbc190de589f0c4118409bf73
logicfirst <slee@logicfirstllc.com>
2021-03-01 20:12:23 -0500

    scott-0301-2015-started some components and styling

---
+++ fantasy-congress/src/Components/FantasyCongress.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+import Home from './Home'
+
+class FantasyCongress extends Component {
+
+  render(){
+    return(
+      <div>
+        <h1>Fantasy Congress</h1>
+      </div>
+    )
+
+  }
+}
+export default FantasyCongress
